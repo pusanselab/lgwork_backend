9e8590a3fa7b62cbc707d5583101bdd899254055
dotols <lim00010@naver.com>
2019-09-16 22:48:07 +0900

    Graph API Complete

--- controller/lg/index.js
+++ controller/lg/index.js
@@ -25,5 +25,13 @@ router.post('/data_search', ctrl.data_search)    // 테스트용
 router.post('/data_search_detail', ctrl.data_search_detail)    // 테스트용
 router.post('/graph_odu', ctrl.graph_odu)    // 테스트용
 router.post('/graph_idu', ctrl.graph_idu)    // 테스트용
+router.post('/graph_hru', ctrl.graph_hru)    // 테스트용
+router.post('/graph_calolimeter', ctrl.graph_calolimeter)    // 테스트용
+router.post('/graph_sidu_casecade', ctrl.graph_sidu_casecade)    // 테스트용
+router.post('/graph_sidu_dxc', ctrl.graph_sidu_dxc)    // 테스트용
+router.post('/graph_sidu_fau', ctrl.graph_sidu_fau)    // 테스트용
+router.post('/graph_sidu_showcase', ctrl.graph_sidu_showcase)    // 테스트용
+router.post('/graph_sidu_whu', ctrl.graph_sidu_whu)    // 테스트용
+
 
 module.exports = router
